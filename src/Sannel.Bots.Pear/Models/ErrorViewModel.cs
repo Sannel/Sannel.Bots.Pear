df9947b499a1197f1ec83fe605263895845d1c7b
Adam Holt <holtsoftware@outlook.com>
2022-10-23 19:05:10 -0600

    Inital

---
+++ src/Sannel.Bots.Pear/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sannel.Bots.Pear.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
